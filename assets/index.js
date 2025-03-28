da90e6964884350885d37d8d6b9c201a0871172b
EssaTereza <tojapulm@gmail.com>
2025-03-28 18:45:37 +0100

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -57,7 +57,7 @@ imageInput.addEventListener('change', (event) => {
     fetch('	https://api.imgur.com/3/image' ,{
         method: 'POST',
         headers: {
-            'Authorization': ' c8c28d402435402'
+            'Authorization': 'Client-ID c8c28d402435402'
         },
         body: data
     })
